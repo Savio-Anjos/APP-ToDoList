47357737a27df99d86148aae15023eeecf998fd4
Savio Anjos <savioanjos2020@gmail.com>
2024-07-11 23:27:26 -0300

    feat :sarkles: Added component header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    maxHeight: 200,
+    backgroundColor: "#0D0D0D",
+    padding: 10,
+    marginTop: 29,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 160,
+    height: 110,
+    resizeMode: "contain",
+  },
+});
